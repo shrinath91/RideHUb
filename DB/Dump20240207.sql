cc0c457dca1ff39c8a0650fe762127465285122a
shrinath91 <shrinathgarad91@gmail.com>
2024-02-07 09:34:55 +0530

    Create Dump20240207.sql

---
+++ DB/Dump20240207.sql
@@ -0,0 +1,175 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ridehub
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`admin_id`),
+  KEY `fk_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_userId` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `car_id` int NOT NULL AUTO_INCREMENT,
+  `driver_id` int NOT NULL,
+  `reg_date` datetime NOT NULL,
+  `licensePlate` varchar(45) NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `make` varchar(45) NOT NULL,
+  `colour` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`car_id`),
+  UNIQUE KEY `licensePlate_UNIQUE` (`licensePlate`),
+  KEY `fk_driver_id_idx` (`driver_id`),
+  CONSTRAINT `fk_driver_id` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`driver_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `drivers`
+--
+
+DROP TABLE IF EXISTS `drivers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `drivers` (
+  `driver_id` int NOT NULL,
+  `driver_name` varchar(45) NOT NULL,
+  `contact` decimal(10,0) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `joinigDate` date NOT NULL,
+  `last_active_date` date DEFAULT NULL,
+  `car_id` int NOT NULL,
+  `userId` int NOT NULL,
+  `rating` decimal(2,1) DEFAULT NULL,
+  PRIMARY KEY (`driver_id`),
+  UNIQUE KEY `contact_UNIQUE` (`contact`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_carid_idx` (`car_id`),
+  KEY `fk_user_id_idx` (`userId`),
+  CONSTRAINT `fk_car_id` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`userId`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `passengers`
+--
+
+DROP TABLE IF EXISTS `passengers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `passengers` (
+  `pass_Id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `contact` decimal(10,0) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `rating` decimal(2,1) DEFAULT NULL,
+  `prefferred_lang` varchar(45) DEFAULT NULL,
+  `emergency_contact` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`pass_Id`),
+  UNIQUE KEY `contact_UNIQUE` (`contact`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_user_id1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ride_passengers`
+--
+
+DROP TABLE IF EXISTS `ride_passengers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ride_passengers` (
+  `ride_id` int NOT NULL,
+  `passenger_id` int NOT NULL,
+  PRIMARY KEY (`ride_id`,`passenger_id`),
+  KEY `fk_ridePassenger_passId_idx` (`passenger_id`),
+  CONSTRAINT `fk_ridePassenger_passId` FOREIGN KEY (`passenger_id`) REFERENCES `passengers` (`pass_Id`),
+  CONSTRAINT `fk_ridePassenger_rideId` FOREIGN KEY (`ride_id`) REFERENCES `rides` (`rideId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `rides`
+--
+
+DROP TABLE IF EXISTS `rides`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rides` (
+  `rideId` int NOT NULL AUTO_INCREMENT,
+  `driver_id` int NOT NULL,
+  `start_location` varchar(45) NOT NULL,
+  `end_location` varchar(45) NOT NULL,
+  `ride_time` datetime NOT NULL,
+  `fare` decimal(5,2) NOT NULL,
+  `total_capacity` int NOT NULL,
+  `current_passenger` int NOT NULL DEFAULT '0',
+  `ride_status` varchar(10) NOT NULL DEFAULT 'ongoing',
+  PRIMARY KEY (`rideId`),
+  KEY `fk_driver_id_idx` (`driver_id`),
+  CONSTRAINT `fk_driver_id1` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`driver_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-07  9:32:30
