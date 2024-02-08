32a330afb06337dc5b6b043332002cf19231a3ce
shrinath91 <shrinathgarad91@gmail.com>
2024-02-08 09:06:05 +0530

    in db added login table

---
+++ DB/Dump20240208.sql
@@ -0,0 +1,212 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ridehub
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `car_id` int NOT NULL AUTO_INCREMENT,
+  `driver_id` int NOT NULL,
+  `reg_date` datetime NOT NULL,
+  `licensePlate` varchar(45) NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `make` varchar(45) NOT NULL,
+  `colour` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`car_id`),
+  UNIQUE KEY `licensePlate_UNIQUE` (`licensePlate`),
+  KEY `fk_driver_id_idx` (`driver_id`),
+  CONSTRAINT `fk_driver_id` FOREIGN KEY (`driver_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars`
+--
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+INSERT INTO `cars` VALUES (2,2,'2024-02-07 22:06:37','ABC1234','Model XYZ','Make ABC','Red'),(3,1,'2024-02-07 22:07:27','Honda','Model XYZ','Make ABC','Red'),(4,2,'2024-02-07 22:31:07','mh12xxx','Wagon-r','suzuki','Red');
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `login` (
+  `login_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `login_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `logout_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`login_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `login_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ride_passengers`
+--
+
+DROP TABLE IF EXISTS `ride_passengers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ride_passengers` (
+  `ride_id` int NOT NULL,
+  `passenger_id` int NOT NULL,
+  PRIMARY KEY (`ride_id`,`passenger_id`),
+  KEY `fk_ridePassenger_passId_idx` (`passenger_id`),
+  CONSTRAINT `fk_ridePassenger_passId` FOREIGN KEY (`passenger_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `fk_ridePassenger_rideId` FOREIGN KEY (`ride_id`) REFERENCES `rides` (`rideId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ride_passengers`
+--
+
+LOCK TABLES `ride_passengers` WRITE;
+/*!40000 ALTER TABLE `ride_passengers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ride_passengers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rides`
+--
+
+DROP TABLE IF EXISTS `rides`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rides` (
+  `rideId` int NOT NULL AUTO_INCREMENT,
+  `driver_id` int NOT NULL,
+  `start_location` varchar(45) NOT NULL,
+  `end_location` varchar(45) NOT NULL,
+  `ride_time` datetime NOT NULL,
+  `fare` decimal(5,2) NOT NULL,
+  `total_capacity` int NOT NULL,
+  `current_passenger` int NOT NULL DEFAULT '0',
+  `ride_status` varchar(10) NOT NULL DEFAULT 'ongoing',
+  PRIMARY KEY (`rideId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rides`
+--
+
+LOCK TABLES `rides` WRITE;
+/*!40000 ALTER TABLE `rides` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rides` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `trans_id` int NOT NULL AUTO_INCREMENT,
+  `trans_date` datetime NOT NULL,
+  `ammount` decimal(5,2) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `sender_id` int NOT NULL,
+  `recipient_id` int NOT NULL,
+  PRIMARY KEY (`trans_id`),
+  KEY `fk_recipient_id_idx` (`recipient_id`),
+  KEY `fk_sender_id_idx` (`sender_id`),
+  CONSTRAINT `fk_recipient_id` FOREIGN KEY (`recipient_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `fk_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `fname` varchar(45) NOT NULL,
+  `lname` varchar(45) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `car_id` int DEFAULT NULL,
+  `rating` decimal(2,1) DEFAULT '5.0',
+  `emergency_contact` varchar(10) NOT NULL,
+  `role` int NOT NULL,
+  `status` varchar(10) DEFAULT 'active',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `contact_UNIQUE` (`contact`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_u_car_id_idx` (`car_id`),
+  CONSTRAINT `fk_u_car_id` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'shri','shrinath123','Shrinath','','989893294','abc@gmail.com','Pune',NULL,5.1,'6231984701',1,'active'),(2,'new_user','password123','New User','','9876543210','new_user@example.com','New Address',4,5.0,'1234567890',1,'active');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-08  9:00:58
