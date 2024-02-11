aa07e5b618033509c964ce0e9e99157cf443df07
Omkar0109 <sobp9999@gmail.com>
2024-02-11 11:28:14 +0530

    Register

---
+++ RentToRideSpringBoot/src/main/java/com/example/demo/controller/CarControllers.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Car;
+import com.example.demo.services.CarService;
+
+	@RestController
+	public class CarControllers {
+		@Autowired
+		CarService cser;
+		
+		@PostMapping("/save_car")
+		public Car Save(@RequestBody Car c)
+		{
+			return cser.Save(c);
+		}
+		
+		@RequestMapping("/getAll")
+		public List<Car> carInfo()
+		{
+			return cser.getAll();
+		}
+	}
